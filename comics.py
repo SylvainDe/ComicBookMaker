eb0d822d0538de8f9f5d731b2f376f4964523cfb
Sylvain <sylvain.desodt+github@gmail.com>
2017-10-11 12:23:51 +0200

    Fix navigation for GoComic

--- comics.py
+++ comics.py
@@ -4714,8 +4714,8 @@ class GenericGoComic(GenericNavigableComic):
     @classmethod
     def get_navi_link(cls, last_soup, next_):
         """Get link to next or previous comic."""
-        PREV = 'fa btn btn-outline-default btn-circle fa-caret-left sm '
-        NEXT = 'fa btn btn-outline-default btn-circle fa-caret-right hidden-sm-up sm '
+        PREV = 'fa btn btn-outline-default btn-circle fa-caret-left js-previous-comic sm '
+        NEXT = 'fa btn btn-outline-default btn-circle fa-caret-right js-next-comic hidden-sm-up sm '
         return last_soup.find('a', class_=NEXT if next_ else PREV)
 
     @classmethod
